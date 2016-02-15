db38ec9e8cbb1bc769e4d86e47be53ceb1ff0e4f
Sposito <sposito.thiago@gmail.com>
2016-02-15 12:18:03 -0200

    DragablePiece prototype #1

---
+++ Assets/PieceDrag.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceDrag : MonoBehaviour {
+	
+	SpriteRenderer sr;
+	void Start () {
+		print (Physics.queriesHitTriggers);
+	}
+	
+	// Update is called once per frame
+	void OnMouseDrag () {
+		
+		Vector3 v3 = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		v3.z = 0;
+		transform.position = v3;
+		}
+
+
+
+
+
+}
