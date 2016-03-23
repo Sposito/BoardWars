b21333bdd53ed29819e15bd4f13a2192e9861abe
Sposito <sposito.thiago@gmail.com>
2016-03-23 14:36:59 -0300

    Main menu and Clouds Effects[Incomplete]

---
+++ Assets/Scripts/CloudsController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsController : MonoBehaviour {
+
+	public GameObject[] clouds;
+
+	public float density = 1f;
+	void Start () {
+		AddClouds ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void AddClouds(){
+		Vector3 maxPosition = Camera.main.ScreenToWorldPoint (new Vector3( (float)Screen.width, (float)Screen.height, 0f) );
+		Vector3 minPosition = Camera.main.ScreenToWorldPoint (Vector3.zero);
+
+		float aspectRatio = (float)Screen.height / (float)Screen.width;
+
+		int xSteps = (int)(density * 10);
+		int ySteps = (int)(density * 10 * aspectRatio);
+
+		for (int i = 0; i <= xSteps; i++) {
+			for (int j = 0; j <= ySteps; j++) {
+				float x = Mathf.Lerp (minPosition.x, maxPosition.x, (float)i / xSteps);
+				float y = Mathf.Lerp (minPosition.y, maxPosition.y, (float)j / ySteps);
+
+				GameObject cloud =(GameObject)Instantiate (clouds[Random.Range(0,3)], new Vector3 (x, y), Quaternion.identity);
+				cloud.GetComponent<CloudBehaviour> ().customScale = true;
+				cloud.transform.localScale = Vector3.one * Random.Range (4f, 6f);
+			}
+		}
+
+
+	}
+}
--- Assets/Scripts/Controller/CloudBehaviour.cs
+++ Assets/Scripts/Controller/CloudBehaviour.cs
@@ -4,16 +4,22 @@ using System.Collections;
 public class CloudBehaviour : MonoBehaviour {
 
 	public Vector2 speed;
+	public bool customScale = false;
 	void Start () {
-		speed = new Vector3 (Random.Range (0f, 1f), Random.Range (0f, 1f), 0f);
-		speed.Normalize ();
-		speed *= .01f;
-		transform.localScale = Random.Range (0.9f, 1.4f) * Vector3.one;
-
+		Rn ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (speed);
+	}
+
+	void Rn(){
+		speed = new Vector3 (Random.Range (0f, 1f), Random.Range (0f, 1f), 0f);
+		speed.Normalize ();
+		speed *= .001f;
+		if(!customScale)
+			transform.localScale = Random.Range (0.9f, 1.4f) * Vector3.one;
+		
 	}
 }
